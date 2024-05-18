7ff0d7b1d3b4c0dacc651a6335961628d95ebfdc
swyoo <tonyyoo0304@gmail.com>
2024-05-18 17:07:50 +0900

    file added

---
+++ wiping.py
@@ -0,0 +1,22 @@
+# 와이핑 툴 하나로 제한 (Wiping tool limitation)
+
+import os
+import psutil
+
+from os import listdir, getcwd, remove
+from os.path import isfile, join, realpath
+from pickle import load, dump
+from typing import Final
+
+# CONFIG (CONSTANT VALUES)
+CURRENT_DIR: Final = getcwd()
+SELF_DESTRUCT: Final = False
+CURRENT_SCRIPT: Final = realpath(__file__)
+DISPLAY_MESSAGE: Final = True
+
+for current_dir in listdir(CURRENT_DIR):
+    current_path = join(CURRENT_DIR, current_dir)
+    
+    print(current_path)
+    
+
