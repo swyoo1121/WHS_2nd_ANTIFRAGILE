c3b08dd9a95d150e031eca2715415e3764017a30
윤혜준 <140903134+mu-pamang@users.noreply.github.com>
2024-05-19 11:21:52 +0900

    data_del.py

--- algs/data_del.py
+++ algs/data_del.py
@@ -6,11 +6,11 @@ def detect_delete_type(file_path):
         file.seek(22)
         flag = file.read(2)
 
-        # Flag 값과 0x0001을 AND 연산하여 결과가 0이면 파일이 삭제된 상태(일반 삭제)
+        # Flag 값과 0x0001을 AND 연산하여 결과가 0이면 파일이 삭제된 상태
         if int.from_bytes(flag, byteorder='little') & 0x0001 == 0:
-            return "일반 삭제"
-        else:
             return "완전 삭제"
+        else:
+            return "일반 삭제"
 
 def main():
     #directory = "C:\\Users\\hj021\\Desktop\\test"
