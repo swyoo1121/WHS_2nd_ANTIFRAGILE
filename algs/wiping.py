5a634fb04b24fba6dbe8a21de2fa5a0a8eb9fce4
swyoo <tonyyoo0304@gmail.com>
2024-07-02 21:52:48 +0900

    0702 update #2

--- algs/wiping.py
+++ algs/wiping.py
@@ -1,14 +1,13 @@
 import os
 
-target_wiped_file_path = r'D:\wiping\$J.copy2'
+target_wiped_file_path = r"C:\Users\hj021\Desktop\final\$J.copy0"
 excel_signature = bytes.fromhex("78 00 6C 00 73 00 78 00")  # x.l.s.x
 j_record_signature = bytes.fromhex("00 00 02 00 00 00")
 CHUNK_SIZE = 4096  # Adjust the chunk size as needed
 
 def detect_wiped(file_path):
     if not os.path.isfile(file_path):
-        print(f"File not found: {file_path}")
-        return
+        return f"{file_path}, File not found"
         
     wiped_checker = False
     file_offset = 0
@@ -16,6 +15,7 @@ def detect_wiped(file_path):
     new_counter = 0
     
     result_wiped = False
+    wiping_trace = []
     
     with open(file_path, "rb") as file:
         while True:
@@ -57,12 +57,12 @@ def detect_wiped(file_path):
                         if excel_signature in next_chunk:
                             counter += 1
                         else:
-                            print("Excel signature not found in the log record")
+                            wiping_trace.append("와이핑 흔적 발견")
                             wiped_checker = True
                             result_wiped = True
                             break  # Exit inner loop if wiped condition is confirmed
                     else:
-                        print("Invalid J record signature found")
+                        wiping_trace.append("Invalid J record signature found")
                 
                 # Look for the next occurrence of Excel signature in the same chunk
                 excel_index = chunk.find(excel_signature, excel_index + len(excel_signature))
@@ -76,13 +76,13 @@ def detect_wiped(file_path):
 
         # Check if wiped_checker is False after processing
         if not wiped_checker:
-            print("File does not appear to be wiped.")
+            wiping_trace.append("File does not appear to be wiped.")
             
-    return result_wiped
-
+    return f"{file_path}, {'; '.join(wiping_trace)}"
 
 def main():
-    detect_wiped(target_wiped_file_path)
+    result = detect_wiped(target_wiped_file_path)
+    print(result)
 
 if __name__ == "__main__":
     main()
